f106a92251d9f4f5001afb03438b1d77b5c2ab00
Wataru.Kasahara <kasahara1019@gmail.com>
2020-07-18 17:04:14 +0900

    feat: Add environment templates for Fireabase
      ref. https://github.com/angular/angularfire/blob/master/docs/install-and-setup.md#3-add-firebase-config-to-environments-variable

---
+++ src/environments/templates/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: '<%= KANBAN_API_KEY %>',
+    authDomain: '<%= KANBAN_AUTH_DOMAIN %>',
+    databaseURL: '<%= KANBAN_DATABASE_URL %>',
+    projectId: '<%= KANBAN_PROJECT_ID %>',
+    storageBucket: '<%= KANBAN_STORAGE_BUCKET %>',
+    messagingSenderId: '<%= KANBAN_MESSAGING_SENDER_ID %>',
+  },
+};
