73f59ffbebd76df04a432c980502d8ea792a4704
Wataru.Kasahara <kasahara1019@gmail.com>
2020-07-18 17:28:41 +0900

    feat: Implement authenticator using Firebase

--- src/app/infrastructures/adapters/authenticator.spec.ts
+++ src/app/infrastructures/adapters/authenticator.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-
+import { AngularFireModule } from '@angular/fire';
 import { Authenticator } from './authenticator';
+
+// NOTE: テスト用のダミーデータ
+//       ref. https://github.com/angular/angularfire/blob/master/docs/install-and-setup.md#3-add-firebase-config-to-environments-variable
+const firebaseEnv = { apiKey: 'dummy', authDomain: '', databaseURL: '', projectId: '', storageBucket: '', messagingSenderId: '' };
 
 describe('Authenticator', () => {
   let authenticator: Authenticator;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [AngularFireModule.initializeApp(firebaseEnv)],
+    });
     authenticator = TestBed.inject(Authenticator);
   });
 
--- src/app/infrastructures/adapters/authenticator.ts
+++ src/app/infrastructures/adapters/authenticator.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import { auth } from 'firebase';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Authenticator {
-  constructor() {}
+  constructor(private angularFireAuth: AngularFireAuth) {}
+
+  loggedInUser$: Observable<firebase.User | null> = this.angularFireAuth.user;
+
+  login() {
+    this.angularFireAuth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.angularFireAuth.signOut();
+  }
 }
