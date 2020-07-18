a1f97cf786c101f0e46923deea013e8db7243e6c
Wataru.Kasahara <kasahara1019@gmail.com>
2020-07-18 17:16:10 +0900

    feat: Initialize for AngularFire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgWorkboxComponentsModule } from '@kasaharu/ng-workbox/components';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './features/home/home.module';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, NgWorkboxComponentsModule, HomeModule],
+  imports: [BrowserModule, AppRoutingModule, AngularFireModule.initializeApp(environment.firebase), NgWorkboxComponentsModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
