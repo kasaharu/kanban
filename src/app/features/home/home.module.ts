05ca896a3879ba71adf0bb97bbed32e72d8926e6
Wataru.Kasahara <kasahara1019@gmail.com>
2020-07-18 16:02:55 +0900

    yarn ng g c features/home/pages/home

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
   ],
