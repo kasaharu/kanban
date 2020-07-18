8586644f66b8d9b53cb50b72f3802f4904a43d94
Wataru.Kasahara <kasahara1019@gmail.com>
2020-07-18 15:50:50 +0900

    feat: Implement header layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgWorkboxComponentsModule } from '@kasaharu/ng-workbox/components';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, NgWorkboxComponentsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
