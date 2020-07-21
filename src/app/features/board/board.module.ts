24b2d215b0d5fda60765e5795ffaa39edd12223a
Wataru.Kasahara <kasahara1019@gmail.com>
2020-07-21 21:08:12 +0900

    yarn ng g c features/board/containers/sections

--- src/app/features/board/board.module.ts
+++ src/app/features/board/board.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SectionsComponent } from './containers/sections/sections.component';
 import { BoardComponent } from './pages/board/board.component';
 
 @NgModule({
-  declarations: [BoardComponent],
+  declarations: [BoardComponent, SectionsComponent],
   imports: [CommonModule],
   exports: [BoardComponent],
 })
---
+++ src/app/features/board/containers/sections/sections.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sections',
+  templateUrl: './sections.component.html',
+  styleUrls: ['./sections.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SectionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
