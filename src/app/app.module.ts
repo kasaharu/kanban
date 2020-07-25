fcce0dddc9aa16f1e290be88b25354b3a30161a5
Wataru.Kasahara <kasahara1019@gmail.com>
2020-07-25 19:26:20 +0900

    feat: Initialize for NgRx Devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { AngularFireModule } from '@angular/fire';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgWorkboxComponentsModule } from '@kasaharu/ng-workbox/components';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,6 +19,7 @@ import { HeaderComponent } from './shared/components/header/header.component';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({ maxAge: 25 }),
     NgWorkboxComponentsModule,
     AppShellModule,
     HomeModule,
