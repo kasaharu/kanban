058cb15b43fde92dc808d464e21d63c9b20e82ba
Wataru.Kasahara <kasahara1019@gmail.com>
2020-07-26 16:58:15 +0900

    feat: Sort sections by orderId

--- src/app/features/board/applications/board.query.ts
+++ src/app/features/board/applications/board.query.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { filter, map } from 'rxjs/operators';
 import { selectStore } from '../store/board.store';
 
 @Injectable({
@@ -8,5 +9,9 @@ import { selectStore } from '../store/board.store';
 export class BoardQuery {
   constructor(private store: Store<{}>) {}
 
-  sections$ = selectStore(this.store, (state) => state.sections);
+  // TODO: orderId 順にソートするロジックは domain 層に移動する
+  sections$ = selectStore(this.store, (state) => state.sections).pipe(
+    filter((sections) => sections.length !== 0),
+    map(([...sections]) => sections.sort((a, b) => a.orderId - b.orderId)),
+  );
 }
