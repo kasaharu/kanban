59d4863f126f0da6713b866a4a244328a3236f22
Wataru.Kasahara <kasahara1019@gmail.com>
2020-07-26 14:29:54 +0900

    feat: Not display until the app is ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppInitializeUsecase } from './features/app-shell/applications/app-initialize.usecase';
+import { AppShellQuery } from './features/app-shell/applications/app-shell.query';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,10 @@ import { AppInitializeUsecase } from './features/app-shell/applications/app-init
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private usecase: AppInitializeUsecase) {}
+  constructor(private query: AppShellQuery, private usecase: AppInitializeUsecase) {}
 
   title = 'kanban';
+  readyApp$ = this.query.readyApp$;
 
   ngOnInit() {
     this.usecase.initialize();
--- src/app/features/app-shell/applications/app-shell.query.ts
+++ src/app/features/app-shell/applications/app-shell.query.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectStore } from '../store/app-shell.store';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppShellQuery {
-  constructor() {}
+  constructor(private store: Store<{}>) {}
+
+  readyApp$ = selectStore(this.store, (state) => state.readyApp);
 }
