53b4e9c0d8f7095ca58e1a15ef3e87c8cab9a280
Wataru.Kasahara <kasahara1019@gmail.com>
2020-07-26 15:40:04 +0900

    feat: Implement the function for fetching collection from Firebase

--- src/app/infrastructures/adapters/database.adapter.ts
+++ src/app/infrastructures/adapters/database.adapter.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DatabaseAdapter {
-  constructor() {}
+  constructor(private db: AngularFirestore) {}
+
+  fetchCollectionWhere<T>(collectionName: string, where: { key: string; value: string }): Observable<T[]> {
+    return this.db
+      .collection<T>(collectionName, (ref) => ref.where(where.key, '==', where.value))
+      .valueChanges();
+  }
 }
