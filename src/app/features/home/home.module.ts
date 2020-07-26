a4181612cfb402af2793f5e47600e240d67c9700
Wataru.Kasahara <kasahara1019@gmail.com>
2020-07-26 13:48:21 +0900

    fix: Link to board

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, BrowserModule, RouterModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
