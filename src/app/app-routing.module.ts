d207d1409843b6e82b1a3db1e05affef1e471dd7
Wataru.Kasahara <kasahara1019@gmail.com>
2020-07-26 14:07:50 +0900

    feat: Redirect to home if not logged in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
+import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 import { RouterModule, Routes } from '@angular/router';
 import { BoardComponent } from './features/board/pages/board/board.component';
 import { HomeComponent } from './features/home/pages/home/home.component';
 
+const redirectUnauthorizedToHome = () => redirectUnauthorizedTo(['home']);
+
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'board', component: BoardComponent },
+  { path: 'board', component: BoardComponent, ...canActivate(redirectUnauthorizedToHome) },
 ];
 
 @NgModule({
