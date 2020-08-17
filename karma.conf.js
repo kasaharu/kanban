6b49e24483812375ebe60e89ccf50bfd77a7cea5
Wataru.Kasahara <kasahara1019@gmail.com>
2020-08-17 21:53:35 +0900

    test: Change to not handle as an error if coverage is not enough

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -20,7 +20,7 @@ module.exports = function(config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
       thresholds: {
-        emitWarning: false,
+        emitWarning: true,
         global: {
           statements: 100,
           lines: 100,
