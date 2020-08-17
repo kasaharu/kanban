7ebb09f633a49427e034094aee5bfc5851201b01
Wataru.Kasahara <kasahara1019@gmail.com>
2020-08-17 20:37:59 +0900

    test: Fix build error when execute test

--- src/app/features/board/store/board.store.spec.ts
+++ src/app/features/board/store/board.store.spec.ts
@@ -1,12 +1,12 @@
 import { Section } from '../../../domain/models';
-import reducer, { actions, State } from './board.store';
+import reducer, { actions, initialState, State } from './board.store';
 
 describe('board reducer', () => {
   it('action type : saveBoard', () => {
-    const state: State = { sections: [] };
+    const state: State = initialState;
     const updatedState: Section[] = [{ id: '1', userId: '1', name: 'test', orderId: 1 }];
     const result = reducer(state, actions.saveSections({ sections: updatedState }));
 
-    expect(result).toEqual({ sections: updatedState });
+    expect(result).toEqual({ ...state, sections: updatedState });
   });
 });
