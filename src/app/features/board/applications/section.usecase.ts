251b103a5474e4c7f41edb0ad50bd5d3c41320d3
Wataru.Kasahara <kasahara1019@gmail.com>
2020-08-23 15:01:29 +0900

    feat: Define the collection name in domain

--- src/app/features/board/applications/section.usecase.ts
+++ src/app/features/board/applications/section.usecase.ts
@@ -4,6 +4,8 @@ import { take } from 'rxjs/operators';
 import { Section, SectionHasTasks, Task } from '../../../domain/models';
 import { DatabaseAdapter } from '../../../infrastructures/adapters/database.adapter';
 import { selectStore as selectAppShellStore } from '../../app-shell/store/app-shell.store';
+import * as SectionDomain from '../domain/section';
+import * as TaskDomain from '../domain/task';
 import { actions, selectStore } from '../store/board.store';
 
 @Injectable({
@@ -21,7 +23,10 @@ export class SectionUsecase {
       return;
     }
 
-    const sections$ = this.databaseAdapter.fetchCollectionWhere<Section>('sections', { key: 'userId', value: loggedInUser.uid });
+    const sections$ = this.databaseAdapter.fetchCollectionWhere<Section>(SectionDomain.COLLECTION_NAME, {
+      key: 'userId',
+      value: loggedInUser.uid,
+    });
     const sections = await sections$.pipe(take(1)).toPromise();
     this.store.dispatch(actions.saveSections({ sections }));
 
@@ -29,7 +34,7 @@ export class SectionUsecase {
   }
 
   private async fetchTasks(userId: string) {
-    const tasks$ = this.databaseAdapter.fetchCollectionWhere<Task>('tasks', { key: 'userId', value: userId });
+    const tasks$ = this.databaseAdapter.fetchCollectionWhere<Task>(TaskDomain.COLLECTION_NAME, { key: 'userId', value: userId });
     const tasks = await tasks$.pipe(take(1)).toPromise();
     this.store.dispatch(actions.saveTasks({ tasks }));
   }
@@ -46,7 +51,7 @@ export class SectionUsecase {
     const sections: Section[] = await selectStore(this.store, (state) => state.sections)
       .pipe(take(1))
       .toPromise();
-    const createdSection = await this.databaseAdapter.createDocument<Section>('sections', {
+    const createdSection = await this.databaseAdapter.createDocument<Section>(SectionDomain.COLLECTION_NAME, {
       userId: user.uid,
       name: addingSection.name,
       orderId: sections.length + 1,
@@ -64,7 +69,7 @@ export class SectionUsecase {
     });
 
     // NOTE: 対象の Section を削除
-    const deletedSectionId = await this.databaseAdapter.deleteDocument<Section>('sections', section.id);
+    const deletedSectionId = await this.databaseAdapter.deleteDocument<Section>(SectionDomain.COLLECTION_NAME, section.id);
     this.store.dispatch(actions.deleteSection({ sectionId: deletedSectionId }));
   }
 
@@ -77,7 +82,7 @@ export class SectionUsecase {
       return;
     }
 
-    const createdTask = await this.databaseAdapter.createDocument<Task>('tasks', {
+    const createdTask = await this.databaseAdapter.createDocument<Task>(TaskDomain.COLLECTION_NAME, {
       userId: user.uid,
       name: addingTask.name,
       sectionId: section.id,
@@ -89,26 +94,26 @@ export class SectionUsecase {
   }
 
   async deleteTask(taskId: string) {
-    const deletedTaskId = await this.databaseAdapter.deleteDocument<Task>('tasks', taskId);
+    const deletedTaskId = await this.databaseAdapter.deleteDocument<Task>(TaskDomain.COLLECTION_NAME, taskId);
     this.store.dispatch(actions.deleteTask({ taskId: deletedTaskId }));
   }
 
   moveTask(tasks: Task[]) {
     tasks.forEach((task, index) => {
       const updatedTask: Task = { ...task, orderId: index + 1 };
-      this.databaseAdapter.updateDocument<Task>('tasks', updatedTask, updatedTask.id);
+      this.databaseAdapter.updateDocument<Task>(TaskDomain.COLLECTION_NAME, updatedTask, updatedTask.id);
     });
   }
 
   async transferTask(sourceTasks: Task[], destinationTasks: Task[], destinationSectionId: string) {
     destinationTasks.forEach((task, index) => {
       const updatedTask: Task = { ...task, orderId: index + 1, sectionId: destinationSectionId };
-      this.databaseAdapter.updateDocument<Task>('tasks', updatedTask, updatedTask.id);
+      this.databaseAdapter.updateDocument<Task>(TaskDomain.COLLECTION_NAME, updatedTask, updatedTask.id);
     });
 
     sourceTasks.forEach((task, index) => {
       const updatedTask: Task = { ...task, orderId: index + 1 };
-      this.databaseAdapter.updateDocument<Task>('tasks', updatedTask, updatedTask.id);
+      this.databaseAdapter.updateDocument<Task>(TaskDomain.COLLECTION_NAME, updatedTask, updatedTask.id);
     });
   }
 }
