ec5c9d8b17d8bf04a2403e23d5688c1cdef3bc79
Wataru.Kasahara <kasahara1019@gmail.com>
2020-09-06 10:32:56 +0900

    yarn ng g c shared/alert-dialog/presenters/alert-dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,11 @@ import { AppComponent } from './app.component';
 import { AppShellModule } from './features/app-shell/app-shell.module';
 import { BoardModule } from './features/board/board.module';
 import { HomeModule } from './features/home/home.module';
+import { AlertDialogComponent } from './shared/alert-dialog/presenters/alert-dialog/alert-dialog.component';
 import { HeaderComponent } from './shared/presenters/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, AlertDialogComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/shared/alert-dialog/presenters/alert-dialog/alert-dialog.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alert-dialog',
+  templateUrl: './alert-dialog.component.html',
+  styleUrls: ['./alert-dialog.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AlertDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
