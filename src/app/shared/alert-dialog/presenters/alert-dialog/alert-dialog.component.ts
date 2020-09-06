8384f51a5b838a88d74337c2a8f736127722b833
Wataru.Kasahara <kasahara1019@gmail.com>
2020-09-06 10:34:46 +0900

    yarn prettier

--- src/app/shared/alert-dialog/presenters/alert-dialog/alert-dialog.component.ts
+++ src/app/shared/alert-dialog/presenters/alert-dialog/alert-dialog.component.ts
@@ -7,10 +7,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AlertDialogComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
