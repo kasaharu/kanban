feb00690b040f967b265b295ba530259279517ab
Wataru.Kasahara <kasahara1019@gmail.com>
2020-09-06 11:55:27 +0900

    feat: Implement the Alert dialog

--- src/app/shared/alert-dialog/presenters/alert-dialog/alert-dialog.component.ts
+++ src/app/shared/alert-dialog/presenters/alert-dialog/alert-dialog.component.ts
@@ -1,4 +1,13 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { OverlayRef } from '@angular/cdk/overlay';
+import { ChangeDetectionStrategy, Component, Inject, InjectionToken } from '@angular/core';
+
+export type AlertDialogData = {
+  title: string;
+  message: string;
+};
+
+export const OVERLAY_REF = new InjectionToken<OverlayRef>('OVERLAY_REF');
+export const OVERLAY_DATA = new InjectionToken<AlertDialogData>('OVERLAY_DATA');
 
 @Component({
   selector: 'app-alert-dialog',
@@ -6,8 +15,21 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   styleUrls: ['./alert-dialog.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AlertDialogComponent implements OnInit {
-  constructor() {}
+export class AlertDialogComponent {
+  constructor(
+    @Inject(OVERLAY_REF) private readonly overlayRef: OverlayRef,
+    @Inject(OVERLAY_DATA) private readonly alertDialogData: AlertDialogData,
+  ) {}
 
-  ngOnInit(): void {}
+  get title(): string {
+    return this.alertDialogData.title;
+  }
+
+  get message(): string {
+    return this.alertDialogData.message;
+  }
+
+  close() {
+    this.overlayRef.dispose();
+  }
 }
--- src/app/shared/alert-dialog/services/alert-dialog.service.ts
+++ src/app/shared/alert-dialog/services/alert-dialog.service.ts
@@ -1,8 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Overlay } from '@angular/cdk/overlay';
+import { ComponentPortal } from '@angular/cdk/portal';
+import { Injectable, Injector } from '@angular/core';
+import { AlertDialogComponent, AlertDialogData, OVERLAY_DATA, OVERLAY_REF } from '../presenters/alert-dialog/alert-dialog.component';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AlertDialogService {
-  constructor() {}
+  constructor(private readonly overlay: Overlay) {}
+
+  show(title: string, message: string) {
+    const overlayRef = this.overlay.create({
+      positionStrategy: this.overlay.position().global().centerHorizontally().centerVertically(),
+      hasBackdrop: true,
+      scrollStrategy: this.overlay.scrollStrategies.block(),
+    });
+
+    const componentPortal = new ComponentPortal(
+      AlertDialogComponent,
+      null,
+      Injector.create([
+        { provide: OVERLAY_REF, useValue: overlayRef },
+        { provide: OVERLAY_DATA, useValue: { title, message } as AlertDialogData },
+      ]),
+    );
+
+    overlayRef.attach(componentPortal);
+  }
 }
