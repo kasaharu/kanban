504633730eb6eba4535bc73af2e971a645c06097
Wataru.Kasahara <kasahara1019@gmail.com>
2020-09-07 09:48:11 +0900

    test: Fix dependency resolution

--- src/app/shared/alert-dialog/services/alert-dialog.service.spec.ts
+++ src/app/shared/alert-dialog/services/alert-dialog.service.spec.ts
@@ -1,12 +1,14 @@
+import { OverlayModule } from '@angular/cdk/overlay';
 import { TestBed } from '@angular/core/testing';
-
 import { AlertDialogService } from './alert-dialog.service';
 
 describe('AlertDialogService', () => {
   let service: AlertDialogService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [OverlayModule],
+    });
     service = TestBed.inject(AlertDialogService);
   });
 
