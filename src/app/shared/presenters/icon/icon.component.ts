fd74fcf361b532e3cc6042e913e5bf3c3a6eca88
Wataru.Kasahara <kasahara1019@gmail.com>
2020-09-21 08:19:52 +0900

    feat: Implement the IconComponent

--- src/app/shared/presenters/icon/icon.component.ts
+++ src/app/shared/presenters/icon/icon.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { IconNameType, IconSizeType } from '../helpers/icon-properties';
 
 @Component({
   selector: 'app-icon',
@@ -7,10 +8,19 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class IconComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  // NOTE: https://material.io/resources/icons
+  @Input()
+  iconName: IconNameType = 'warning';
+  @Input()
+  lightMode = false;
+  @Input()
+  size: IconSizeType = 24;
 
-  ngOnInit(): void {
+  get colorClass() {
+    return this.lightMode ? 'md-light' : 'md-dark';
   }
 
+  ngOnInit(): void {}
 }
