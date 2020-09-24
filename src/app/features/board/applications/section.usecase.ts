4c4cd673810ea9c5acd9372eed081762b501f482
Wataru.Kasahara <kasahara1019@gmail.com>
2020-09-24 09:59:01 +0900

    fix: Fix duplicate imports

--- src/app/features/board/applications/section.usecase.ts
+++ src/app/features/board/applications/section.usecase.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { take } from 'rxjs/operators';
 import { SectionHasTasks, Task } from '../../../domain/models';
-import * as SectionDomain from '../../../domain/section/section.vo';
-import { Section } from '../../../domain/section/section.vo';
+import { COLLECTION_NAME, Section, SectionValueObject } from '../../../domain/section/section.vo';
 import { DatabaseAdapter } from '../../../infrastructures/adapters/database.adapter';
 import { selectStore as selectAppShellStore } from '../../app-shell/store/app-shell.store';
 import * as TaskDomain from '../domain/task';
@@ -26,10 +25,7 @@ export class SectionUsecase {
       return;
     }
 
-    const sections$ = this.databaseAdapter.fetchCollectionWhere<Section>(SectionDomain.COLLECTION_NAME, {
-      key: 'userId',
-      value: loggedInUser.uid,
-    });
+    const sections$ = this.databaseAdapter.fetchCollectionWhere<Section>(COLLECTION_NAME, { key: 'userId', value: loggedInUser.uid });
     const sections = await sections$.pipe(take(1)).toPromise();
     this.store.dispatch(actions.saveSections({ sections }));
 
@@ -56,8 +52,8 @@ export class SectionUsecase {
       .toPromise();
 
     try {
-      const newerSection = SectionDomain.SectionValueObject.create(addingSection.name, user.uid, sections.length + 1);
-      const createdSection = await this.databaseAdapter.createDocument<Section>(SectionDomain.COLLECTION_NAME, newerSection);
+      const newerSection = SectionValueObject.create(addingSection.name, user.uid, sections.length + 1);
+      const createdSection = await this.databaseAdapter.createDocument<Section>(COLLECTION_NAME, newerSection);
       this.store.dispatch(actions.createSection({ section: createdSection }));
     } catch (_) {
       this.store.dispatch(ErrorStoreActions.setError({ errorType: ErrorTypeEnum.OverSectionNameLength }));
@@ -72,14 +68,14 @@ export class SectionUsecase {
     });
 
     // NOTE: 対象の Section を削除
-    const deletedSectionId = await this.databaseAdapter.deleteDocument<Section>(SectionDomain.COLLECTION_NAME, section.id);
+    const deletedSectionId = await this.databaseAdapter.deleteDocument<Section>(COLLECTION_NAME, section.id);
     this.store.dispatch(actions.deleteSection({ sectionId: deletedSectionId }));
   }
 
   updateSectionName(newName: string, section: SectionHasTasks) {
     try {
-      const updatedSection = SectionDomain.SectionValueObject.create(newName, section.userId, section.orderId, section.id);
-      this.databaseAdapter.updateDocument<Section>(SectionDomain.COLLECTION_NAME, updatedSection.plainObject(), updatedSection.id);
+      const updatedSection = SectionValueObject.create(newName, section.userId, section.orderId, section.id);
+      this.databaseAdapter.updateDocument<Section>(COLLECTION_NAME, updatedSection.plainObject(), updatedSection.id);
       // FIXME: SectionName 変更後に store に更新をかける
     } catch (error) {
       this.store.dispatch(ErrorStoreActions.setError({ errorType: ErrorTypeEnum.OverSectionNameLength }));
