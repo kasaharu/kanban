c38bf7a5bbe038147c24f462e7d655e765ee8428
Wataru.Kasahara <kasahara1019@gmail.com>
2020-12-13 16:28:12 +0900

    fix: Delete selector from Page container

--- src/app/features/board/pages/board/board.component.ts
+++ src/app/features/board/pages/board/board.component.ts
@@ -1,7 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
