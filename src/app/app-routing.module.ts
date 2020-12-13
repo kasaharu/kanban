effa9c05b16bf2f4fca9edfe643089f1e56603d3
Wataru.Kasahara <kasahara1019@gmail.com>
2020-12-13 16:25:52 +0900

    fix: Move pages/ out of ui/

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 import { RouterModule, Routes } from '@angular/router';
-import { BoardComponent } from './features/board/ui/pages/board/board.component';
+import { BoardComponent } from './features/board/pages/board/board.component';
 import { HomePageComponent } from './features/home/pages/home/home.component';
 
 const redirectUnauthorizedToHome = () => redirectUnauthorizedTo(['home']);
