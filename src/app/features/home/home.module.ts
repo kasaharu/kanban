2301db8476bc284da62b779bfe3281acf6ffaa0f
Wataru.Kasahara <kasahara1019@gmail.com>
2020-12-13 15:01:47 +0900

    yarn ng g c features/home/containers/home

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from './containers/home/home.component';
 import { HomePageComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [HomePageComponent, HomeComponent],
   imports: [CommonModule, BrowserModule, RouterModule],
   exports: [HomePageComponent],
 })
