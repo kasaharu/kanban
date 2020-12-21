9a5925c429aaf89e4d8bbb6fa76c86ab0c8697d0
Wataru.Kasahara <kasahara1019@gmail.com>
2020-12-21 20:25:33 +0900

    test: Delete unnecessary test case

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -29,10 +29,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'kanban'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('kanban');
-  });
 });
