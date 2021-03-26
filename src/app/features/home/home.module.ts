ca1c8a57e3790669bde85bd4d8147f656e8c75dc
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-26 21:49:32 +0900

    feat(home): Update home layout

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { HomeComponent } from './containers/home/home.component';
 import { HomePageComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [HomePageComponent, HomeComponent],
-  imports: [CommonModule, BrowserModule, RouterModule],
+  imports: [CommonModule, BrowserModule, RouterModule, SharedModule],
   exports: [HomePageComponent],
 })
 export class HomeModule {}
--- src/app/shared/presenters/helpers/icon-properties.ts
+++ src/app/shared/presenters/helpers/icon-properties.ts
@@ -1,2 +1,2 @@
-export type IconNameType = 'account_circle' | 'expand_more' | 'warning';
+export type IconNameType = 'account_circle' | 'expand_more' | 'login' | 'warning';
 export type IconSizeType = 18 | 24 | 36 | 48;
