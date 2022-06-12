22924c9dc2de2a4498a452847e7a4e8a869c4c91
Wataru.Kasahara <kasahara1019@gmail.com>
2022-06-12 14:37:16 +0900

    feat: BoardPageComponent 専用の ComponentStore class を作成

--- src/app/features/board/pages/board/board.component.ts
+++ src/app/features/board/pages/board/board.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BoardUsecase } from './board.usecase';
 
 @Component({
   // template: `<app-sections></app-sections>`,
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [BoardUsecase],
 })
 export class BoardPageComponent {}
--- src/app/features/board/pages/board/board.usecase.ts
+++ src/app/features/board/pages/board/board.usecase.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { ComponentStore } from '@ngrx/component-store';
+import { Section } from '../../../../domain/section/section.vo';
+import { Task } from '../../../../domain/task/task';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class BoardUsecase {
-  constructor() {}
+export interface BoardState {
+  sections: Section[];
+  tasks: Task[];
+}
+
+export const initialState: BoardState = {
+  sections: [],
+  tasks: [],
+};
+
+@Injectable()
+export class BoardUsecase extends ComponentStore<BoardState> {
+  constructor() {
+    super(initialState);
+  }
 }
