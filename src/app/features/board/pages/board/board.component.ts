6d9cb1b8b73c53d8b5488dea043963e3883f671c
Wataru.Kasahara <kasahara1019@gmail.com>
2022-06-12 14:19:25 +0900

    refactor: BoardPageComponent に html と scss ファイルを配置

--- src/app/features/board/pages/board/board.component.ts
+++ src/app/features/board/pages/board/board.component.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  template: `<app-sections></app-sections>`,
+  // template: `<app-sections></app-sections>`,
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BoardPageComponent {}
