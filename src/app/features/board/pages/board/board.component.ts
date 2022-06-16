01707b16baa6bec4f78c15fc964a293664c3a2b4
Wataru.Kasahara <kasahara1019@gmail.com>
2022-06-16 21:18:19 +0900

    feat: sections と tasks を api 経由で取得

--- src/app/features/board/pages/board/board.component.ts
+++ src/app/features/board/pages/board/board.component.ts
@@ -12,9 +12,11 @@ export class BoardPageComponent implements OnInit {
   constructor(private usecase: BoardUsecase) {}
 
   sections$ = this.usecase.sections$;
+  tasks$ = this.usecase.tasks$;
 
   ngOnInit(): void {
     this.sections$.subscribe((val) => console.log(val));
-    this.usecase.fetchSections();
+    this.tasks$.subscribe((val) => console.log(val));
+    this.usecase.fetchBoardItem();
   }
 }
--- src/app/features/board/pages/board/board.usecase.ts
+++ src/app/features/board/pages/board/board.usecase.ts
@@ -4,9 +4,10 @@ import firebase from 'firebase/compat/app';
 import { take } from 'rxjs/operators';
 import { Section } from '../../../../domain/section/section.vo';
 import { Task } from '../../../../domain/task/task';
-import { extractUserInfo } from '../../../../domain/user/user';
+import { extractUserInfo, User } from '../../../../domain/user/user';
 import { Authenticator } from '../../../../infrastructures/adapters/authenticator';
 import { SectionGateway } from '../../../../infrastructures/gateways/section.gateway';
+import { TaskGateway } from '../../../../infrastructures/gateways/task.gateway';
 
 export interface BoardState {
   sections: Section[];
@@ -20,23 +21,40 @@ export const initialState: BoardState = {
 
 @Injectable()
 export class BoardUsecase extends ComponentStore<BoardState> {
-  constructor(private authenticator: Authenticator, private readonly _sectionGateway: SectionGateway) {
+  constructor(
+    private authenticator: Authenticator,
+    private readonly _sectionGateway: SectionGateway,
+    private readonly _taskGateway: TaskGateway,
+  ) {
     super(initialState);
   }
 
   readonly sections$ = this.select((state) => state.sections);
   readonly saveSections = this.updater((state, sections: Section[]) => ({ ...state, sections }));
 
-  async fetchSections() {
+  readonly tasks$ = this.select((state) => state.tasks);
+  readonly saveTasks = this.updater((state, tasks: Task[]) => ({ ...state, tasks }));
+
+  async fetchBoardItem() {
     const user: firebase.User | null = await this.authenticator.loggedInUser$.pipe(take(1)).toPromise();
     const loggedInUser = extractUserInfo(user);
     if (loggedInUser === null) {
       return;
     }
 
-    // NOTE: section 一覧を取得
+    await this.fetchSections(loggedInUser);
+    await this.fetchTasks(loggedInUser);
+  }
+
+  async fetchSections(loggedInUser: User) {
     const sections$ = this._sectionGateway.getSections(loggedInUser.uid);
     const sections = await sections$.pipe(take(1)).toPromise();
     this.saveSections(sections);
+  }
+
+  async fetchTasks(loggedInUser: User) {
+    const tasks$ = this._taskGateway.getTasks(loggedInUser.uid);
+    const tasks = await tasks$.pipe(take(1)).toPromise();
+    this.saveTasks(tasks);
   }
 }
