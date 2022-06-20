55666242fc4d58ed5b5e34f1161f9ea27f0e2775
Wataru.Kasahara <kasahara1019@gmail.com>
2022-06-20 20:50:37 +0900

    feat: SectionsComponent をもとにボードのレイアウトを表示

--- src/app/features/board/pages/board/board.component.ts
+++ src/app/features/board/pages/board/board.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { BoardUsecase } from './board.usecase';
 
 @Component({
@@ -8,13 +10,27 @@ import { BoardUsecase } from './board.usecase';
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [BoardUsecase],
 })
-export class BoardPageComponent implements OnInit {
+export class BoardPageComponent implements OnInit, OnDestroy {
   constructor(private usecase: BoardUsecase) {}
+
+  private _onDestroy$ = new Subject();
+  private _sectionIds!: string[];
 
   sectionsHasTasks$ = this.usecase.sectionsHasTasks$;
 
   ngOnInit(): void {
-    this.sectionsHasTasks$.subscribe((val) => console.log(val));
+    this.sectionsHasTasks$.pipe(takeUntil(this._onDestroy$)).subscribe((sectionsHasTasks) => {
+      this._sectionIds = sectionsHasTasks.map((x) => x.id);
+    });
     this.usecase.fetchBoardItem();
+  }
+
+  ngOnDestroy(): void {
+    this._onDestroy$.next();
+  }
+
+  // NOTE: task が section をまたいで移動するために必要
+  getConnectedList() {
+    return this._sectionIds;
   }
 }
