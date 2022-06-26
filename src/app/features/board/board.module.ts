aabc65b356eb8f50c5a514b8f395dac800200b44
Wataru.Kasahara <kasahara1019@gmail.com>
2022-06-26 16:14:13 +0900

    refactor: 使わなくなった board feature の applications/ と store/ を削除

--- src/app/features/board/board.module.ts
+++ src/app/features/board/board.module.ts
@@ -2,18 +2,15 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { StoreModule } from '@ngrx/store';
 import { BoardPageComponent } from './pages/board/board.component';
 import { EditableSectionNameComponent } from './presenters/editable-section-name/editable-section-name.component';
 import { NewTaskFormComponent } from './presenters/new-task-form/new-task-form.component';
 import { SectionFormComponent } from './presenters/section-form/section-form.component';
 import { TaskCardComponent } from './presenters/task-card/task-card.component';
-import { boardFeature } from './store/board.store';
-import { errorFeature } from './store/error.store';
 
 @NgModule({
   declarations: [BoardPageComponent, SectionFormComponent, TaskCardComponent, NewTaskFormComponent, EditableSectionNameComponent],
-  imports: [CommonModule, ReactiveFormsModule, DragDropModule, StoreModule.forFeature(boardFeature), StoreModule.forFeature(errorFeature)],
+  imports: [CommonModule, ReactiveFormsModule, DragDropModule],
   exports: [BoardPageComponent],
 })
 export class BoardModule {}
