d3e04c7c5e3baeec34a1f89ea7e578d72349688a
Wataru.Kasahara <kasahara1019@gmail.com>
2022-06-28 21:03:09 +0900

    feat: ログイン情報の管理を AppInitializerService でおこなう

--- src/app/app-initializer.service.ts
+++ src/app/app-initializer.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { ComponentStore } from '@ngrx/component-store';
+import { User } from './domain/user/user';
+
+interface State {
+  loggedInUser: User | null;
+  readyApp: boolean;
+}
+
+const initialState: State = {
+  loggedInUser: null,
+  readyApp: false,
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AppInitializerService {
+export class AppInitializerService extends ComponentStore<State> {
+  constructor() {
+    super(initialState);
+  }
 
-  constructor() { }
+  readonly readyApp$ = this.select((state) => state.readyApp);
+  readonly loggedIn$ = this.select((state) => (state.loggedInUser ? true : false));
+
+  readonly activate = this.updater((state, loggedInUser: User | null) => ({ ...state, loggedInUser, readyApp: true }));
+  readonly login = this.updater((state, loggedInUser: User | null) => ({ ...state, loggedInUser }));
 }
