99734348cf30945333b3d4876165cdd9275849a2
Wataru.Kasahara <kasahara1019@gmail.com>
2022-06-29 19:33:25 +0900

    refactor: 使わなくなった app-shell の store/ を削除

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { StoreModule } from '@ngrx/store';
 import { SharedModule } from '../shared/shared.module';
 import { ProfileIconComponent } from './app-shell/components/profile-icon/profile-icon.component';
 import { HeaderComponent } from './app-shell/containers/header/header.component';
-import { appShellFeature } from './app-shell/store/app-shell.store';
 
 @NgModule({
   declarations: [HeaderComponent, ProfileIconComponent],
-  imports: [CommonModule, StoreModule.forFeature(appShellFeature), RouterModule, SharedModule],
+  imports: [CommonModule, RouterModule, SharedModule],
   exports: [HeaderComponent],
 })
 export class CoreModule {}
