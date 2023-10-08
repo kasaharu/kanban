bc434a1054355be2aa178d654ce2f23d7d52eeaa
Wataru.Kasahara <kasahara1019@gmail.com>
2023-10-08 14:35:40 +0900

    npm run ng generate @angular/core:standalone: Remove unnecessary NgModule classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { BoardModule } from './features/board/board.module';
-import { HomeModule } from './features/home/home.module';
-import { SharedModule } from './shared/shared.module';
+
+
+
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,11 +17,7 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     OverlayModule,
     AngularFireModule.initializeApp(environment.firebase),
-    CoreModule,
-    SharedModule,
-    HomeModule,
-    BoardModule,
-  ],
+],
   providers: [],
   bootstrap: [AppComponent],
 })
