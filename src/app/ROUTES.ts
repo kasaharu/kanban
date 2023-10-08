5c1956c7b41bae41ddbbfa47d01d6ab46fa3f02b
Wataru.Kasahara <kasahara1019@gmail.com>
2023-10-08 15:00:40 +0900

    refactor: RouterModule をやめて provieRouter に変更

--- src/app/ROUTES.ts
+++ src/app/ROUTES.ts
@@ -1,19 +1,14 @@
-import { NgModule } from '@angular/core';
 import { canActivate, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { BoardPageComponent } from './features/board/pages/board/board.component';
 import { HomePageComponent } from './features/home/pages/home/home.component';
 
 const redirectUnauthorizedToHome = () => redirectUnauthorizedTo(['home']);
 
-const routes: Routes = [
+const ROUTES: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
   { path: 'board', component: BoardPageComponent, ...canActivate(redirectUnauthorizedToHome) },
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes, {})],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
+export default ROUTES;
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { OverlayModule } from '@angular/cdk/overlay';
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { AngularFireModule } from '@angular/fire/compat';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { AppRoutingModule } from './app/app-routing.module';
+import { provideRouter } from '@angular/router';
+import ROUTES from './app/ROUTES';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 
@@ -11,5 +12,8 @@ if (environment.production) {
 }
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule, AppRoutingModule, OverlayModule, AngularFireModule.initializeApp(environment.firebase))],
+  providers: [
+    provideRouter(ROUTES),
+    importProvidersFrom(BrowserModule, OverlayModule, AngularFireModule.initializeApp(environment.firebase)),
+  ],
 }).catch((err) => console.error(err));
