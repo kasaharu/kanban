a9088811405988fb443a1e6c2481afe477cf5e39
Wataru.Kasahara <kasahara1019@gmail.com>
2023-10-22 11:03:38 +0900

    refactor: pages から役割に合わせて containers に変更

--- src/app/ROUTES.ts
+++ src/app/ROUTES.ts
@@ -1,6 +1,6 @@
 import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 import { Routes } from '@angular/router';
-import { BoardComponent } from './features/board/pages/board/board.component';
+import { BoardComponent } from './features/board/containers/board/board.component';
 import HomePageComponent from './features/home/home-page.component';
 
 const redirectUnauthorizedToHome = () => redirectUnauthorizedTo(['home']);
