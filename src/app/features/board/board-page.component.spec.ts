c4125ff122ea55bf37031132aa48596e789ef641
Wataru.Kasahara <kasahara1019@gmail.com>
2023-10-29 11:33:12 +0900

    test: page component のテストを RouterTestingHarness を使って書き直し

--- src/app/features/board/board-page.component.spec.ts
+++ src/app/features/board/board-page.component.spec.ts
@@ -1,17 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { RouterTestingHarness } from '@angular/router/testing';
 import BoardPageComponent from './board-page.component';
+import { BoardComponent } from './containers/board/board.component';
+
+@Component({ selector: 'app-board', standalone: true, template: '' })
+class MockBoardComponent {}
 
 describe('BoardPageComponent', () => {
+  let harness: RouterTestingHarness;
   let component: BoardPageComponent;
-  let fixture: ComponentFixture<BoardPageComponent>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({
       imports: [BoardPageComponent],
-    });
-    fixture = TestBed.createComponent(BoardPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      providers: [provideRouter([{ path: 'board', component: BoardPageComponent }])],
+    })
+      .overrideComponent(BoardPageComponent, { remove: { imports: [BoardComponent] }, add: { imports: [MockBoardComponent] } })
+      .compileComponents();
+
+    harness = await RouterTestingHarness.create();
+    component = await harness.navigateByUrl('board', BoardPageComponent);
+
+    harness.detectChanges();
   });
 
   it('should create', () => {
