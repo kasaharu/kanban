0c9991abb91a87865b75fd6b3a0f87bb1b806a91
Wataru.Kasahara <kasahara1019@gmail.com>
2023-10-29 13:10:42 +0900

    refactor: DI した field を soft private から hard private に変更

--- src/app/features/board/containers/board/board.usecase.ts
+++ src/app/features/board/containers/board/board.usecase.ts
@@ -1,10 +1,8 @@
 import { Injectable, inject } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
-import { take } from 'rxjs/operators';
 import { SectionHasTasks } from '../../../../domain/models';
 import { Section, SectionValueObject } from '../../../../domain/section/section.vo';
 import { Task } from '../../../../domain/task/task';
-import { User } from '../../../../domain/user/user';
 import { Authenticator } from '../../../../infrastructures/adapters/authenticator';
 import { SectionGateway } from '../../../../infrastructures/gateways/section.gateway';
 import { TaskGateway } from '../../../../infrastructures/gateways/task.gateway';
@@ -12,52 +10,40 @@ import { BoardStore } from './board.store';
 
 @Injectable()
 export class BoardUsecase {
-  #store = inject(BoardStore);
-
-  constructor(
-    private authenticator: Authenticator,
-    private readonly _sectionGateway: SectionGateway,
-    private readonly _taskGateway: TaskGateway,
-  ) {}
+  readonly #store = inject(BoardStore);
+  readonly #authenticator = inject(Authenticator);
+  readonly #sectionGateway = inject(SectionGateway);
+  readonly #taskGateway = inject(TaskGateway);
 
   async fetchBoardItem() {
-    const loggedInUser = await firstValueFrom(this.authenticator.loggedInUser$);
+    const loggedInUser = await firstValueFrom(this.#authenticator.loggedInUser$);
     if (loggedInUser === null) {
       return;
     }
 
-    await this.fetchSections(loggedInUser);
-    await this.fetchTasks(loggedInUser);
-  }
-
-  async fetchSections(loggedInUser: User) {
-    const sections$ = this._sectionGateway.getSections(loggedInUser.uid);
-    const sections = await firstValueFrom(sections$.pipe(take(1)));
+    const sections = await firstValueFrom(this.#sectionGateway.getSections(loggedInUser.uid));
     this.#store.setSections(sections);
-  }
 
-  async fetchTasks(loggedInUser: User) {
-    const tasks$ = this._taskGateway.getTasks(loggedInUser.uid);
-    const tasks = await firstValueFrom(tasks$.pipe(take(1)));
+    const tasks = await firstValueFrom(this.#taskGateway.getTasks(loggedInUser.uid));
     this.#store.setTasks(tasks);
   }
 
   moveSection(sectionsHasTasks: SectionHasTasks[]) {
     sectionsHasTasks.forEach((sectionHasTasks, index) => {
       const section: Section = { id: sectionHasTasks.id, name: sectionHasTasks.name, userId: sectionHasTasks.userId, orderId: index + 1 };
-      this._sectionGateway.putSection(section);
+      this.#sectionGateway.putSection(section);
     });
   }
 
   async addSection(addingSection: Section) {
-    const loggedInUser = await firstValueFrom(this.authenticator.loggedInUser$);
+    const loggedInUser = await firstValueFrom(this.#authenticator.loggedInUser$);
     if (loggedInUser === null) {
       return;
     }
 
     try {
       const newerSection = SectionValueObject.create(addingSection.name, loggedInUser.uid, this.#store.$sectionsHasTasks().length + 1);
-      const createdSection = await this._sectionGateway.postSection(newerSection.plainObject());
+      const createdSection = await this.#sectionGateway.postSection(newerSection.plainObject());
       this.#store.addSection(createdSection);
     } catch (_) {
       // TODO: セクション名が 15 文字(ErrorTypeEnum.OverSectionNameLength) を超えた場合にエラーメッセージを出す
@@ -74,14 +60,14 @@ export class BoardUsecase {
     });
 
     // NOTE: 対象の Section を削除
-    const deletedSectionId = await this._sectionGateway.deleteSection(section);
+    const deletedSectionId = await this.#sectionGateway.deleteSection(section);
     this.#store.deleteSection(deletedSectionId);
   }
 
   async updateSectionName(newName: string, section: SectionHasTasks) {
     try {
       const updatedSection = SectionValueObject.create(newName, section.userId, section.orderId, section.id);
-      await this._sectionGateway.putSection(updatedSection.plainObject());
+      await this.#sectionGateway.putSection(updatedSection.plainObject());
       this.#store.updateSection(updatedSection);
     } catch (error) {
       // TODO: セクション名が 15 文字(ErrorTypeEnum.OverSectionNameLength) を超えた場合にエラーメッセージを出す
@@ -93,29 +79,29 @@ export class BoardUsecase {
   moveTask(tasks: Task[]) {
     tasks.forEach((task, index) => {
       const updatedTask: Task = { ...task, orderId: index + 1 };
-      this._taskGateway.putTask(updatedTask);
+      this.#taskGateway.putTask(updatedTask);
     });
   }
 
   async transferTask(sourceTasks: Task[], destinationTasks: Task[], destinationSectionId: string) {
     destinationTasks.forEach((task, index) => {
       const updatedTask: Task = { ...task, orderId: index + 1, sectionId: destinationSectionId };
-      this._taskGateway.putTask(updatedTask);
+      this.#taskGateway.putTask(updatedTask);
     });
 
     sourceTasks.forEach((task, index) => {
       const updatedTask: Task = { ...task, orderId: index + 1 };
-      this._taskGateway.putTask(updatedTask);
+      this.#taskGateway.putTask(updatedTask);
     });
   }
 
   async createTask(addingTask: Task, section: SectionHasTasks) {
-    const loggedInUser = await firstValueFrom(this.authenticator.loggedInUser$);
+    const loggedInUser = await firstValueFrom(this.#authenticator.loggedInUser$);
     if (loggedInUser === null) {
       return;
     }
 
-    const createdTask = await this._taskGateway.postTask({
+    const createdTask = await this.#taskGateway.postTask({
       userId: loggedInUser.uid,
       name: addingTask.name,
       sectionId: section.id,
@@ -128,7 +114,7 @@ export class BoardUsecase {
   }
 
   async deleteTask(taskId: string) {
-    const deletedTaskId = await this._taskGateway.deleteTask(taskId);
+    const deletedTaskId = await this.#taskGateway.deleteTask(taskId);
     this.#store.deleteTask(deletedTaskId);
   }
 }
