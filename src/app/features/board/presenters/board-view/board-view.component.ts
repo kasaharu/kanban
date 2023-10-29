0dcfdf945cf415be4169fc3e1b091e97acb62515
Wataru.Kasahara <kasahara1019@gmail.com>
2023-10-29 17:37:16 +0900

    npm run ng g c features/board/presenters/board-view -- --standalone --style=none

---
+++ src/app/features/board/presenters/board-view/board-view.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-board-view',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './board-view.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BoardViewComponent {
+
+}
